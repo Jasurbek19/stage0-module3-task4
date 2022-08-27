2aa16d2106651d662cc4d7bbd0fe6c8dcd94b93f
Jasurbek19 <2jasurbek4020@gmail.com>
2022-08-27 16:04:07 +0500

    1

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int charC=99,charH=104,charA=97,charR=114;
+        System.out.println((char)charC+"\n"+(char)charH+(char)charA+"\n"+(char)charR);
     }
 }
